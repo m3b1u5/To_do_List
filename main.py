f7ccedc2fa2fe53997da4a20ec61810b58fc392d
m3b1u5 <dbatezat@gmail.com>
2024-12-14 11:31:47 +0200

    revised 2

--- main.py
+++ main.py
@@ -4,80 +4,74 @@
 class ToDoList:
 
     def __init__(self):
-        self.__tasks__ = []
+        self._tasks_ = []
 
     def add_task(self, task: str):
         if not len(task):
             print(f"\nИмя задачи не может быть пустым.")
             return
-        self.__tasks__.append({"name": task, "status": False})
+        self._tasks_.append({"name": task, "status": False})
         print(
-            f"\nЗадача \"{self.__tasks__[-1]['name']}\" успешно добавлена в список под номером {len(self.__tasks__)}!.")
+            f"\nЗадача \"{self._tasks_[-1]['name']}\" успешно добавлена в список под номером {len(self._tasks_)}!.")
 
     def complete_task(self, task: int):
-        if not 0 <= task - 1 < len(self.__tasks__):
+        if not 0 <= task - 1 < len(self._tasks_):
             print("\nЗадача не найдена. ")
             return
-        self.__tasks__[task - 1]["status"] = True
-        print(f"\nСтатус задачи \"{self.__tasks__[task - 1]['name']}\" успешно изменен на \"Выполнено\"")
+        self._tasks_[task - 1]["status"] = True
+        print(f"\nСтатус задачи \"{self._tasks_[task - 1]['name']}\" успешно изменен на \"Выполнено\"")
 
     def remove_task(self, task: int):
-        if not 0 <= task - 1 < len(self.__tasks__):
+        if not 0 <= task - 1 < len(self._tasks_):
             print("\nЗадача не найдена. ")
             return
-        task_name = self.__tasks__[task - 1]['name'] or ""
+        task_name = self._tasks_[task - 1]['name'] or ""
         try:
-            del (self.__tasks__[task - 1])
+            del (self._tasks_[task - 1])
             print(f"\nЗадача \"{task_name}\" успешно удалена из списка задач")
         except Exception as e:
             print(f"\nЧто-то пошло не так ... : {e}")
 
     def list_tasks(self):
-        if not self.__tasks__:
+        if not self._tasks_:
             print(f"\nСписок задач пуст. ")
             return
         print(f'\nNo- {"Название задачи": <80} - Выполнено?\n')
-        for i, task in enumerate(self.__tasks__):
+        for i, task in enumerate(self._tasks_):
             status = "✓" if task["status"] else "X"
             print(f'{i + 1}- {task["name"]:_<90} {status}')
-
-
-def prompt():
-    print(f"""\n{'ToDo List':=^50}
-1. Добавить задачу
-2. Вывести список задач на экран
-3. Отметить задачу как выполненную
-4. Удалить задачу из списка
-5. Выход""")
-
-    return input("Выберите действие: ")
 
 
 def main():
     my_task_list = ToDoList()
 
     menu = {
-        1: my_task_list.add_task,
-        2: my_task_list.list_tasks,
-        3: my_task_list.complete_task,
-        4: my_task_list.remove_task
+        1: (my_task_list.add_task, '1. Добавить задачу'),
+        2: (my_task_list.list_tasks, '2. Вывести список задач на экран'),
+        3: (my_task_list.complete_task, '3. Отметить задачу как выполненную'),
+        4: (my_task_list.remove_task, '4. Удалить задачу из списка'),
     }
 
     while True:
         try:
-            action = int(prompt())
+            print(f"\n{'ToDo List':=^50}")
+            for i in menu:
+                print(menu[i][1])
+            print(f"{len(menu) + 1}. Выход")
+
+            action = int(input("Выберите действие: "))
 
             match action:
                 case 1:
                     task = str(input("\nВведите название задачи: "))
-                    menu[action](task)
+                    menu[action][0](task)
                 case 2:
-                    menu[action]()
+                    menu[action][0]()
                 case 3:
                     while True:
                         try:
                             task = int(input("\nВведите номер задачи для выполнения: "))
-                            menu[action](task)
+                            menu[action][0](task)
                             break
                         except ValueError:
                             print("Ошибка, введите число: ")
@@ -85,7 +79,7 @@ def main():
                     while True:
                         try:
                             task = int(input("\nВведите номер задачи для удаления: "))
-                            menu[action](task)
+                            menu[action][0](task)
                             break
                         except ValueError:
                             print("Ошибка, введите число: ")
